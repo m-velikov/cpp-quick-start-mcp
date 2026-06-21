50d4c41b0f90265f0ede5595c68d7b32cfc20952
Momchil Velikov <momchil.velikov@gmail.com>
2026-06-21 16:48:12 +0100

    Replace the deprecated `tseslint.config(...)` helper

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,14 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
+export default defineConfig([
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
-      ],
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
       "@typescript-eslint/no-explicit-any": "warn",
     },
   },
-);
+]);
